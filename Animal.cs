736a641eeb007317b3a0ea1baa597d16c897f491
Asna714 <89193445+Asna714@users.noreply.github.com>
2024-09-30 00:03:16 +0530

    added c#assignment2

---
+++ Animal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+public class Animal
+{
+
+    public string Name { get; set; }
+    
+    public virtual void Sound()
+    {
+        Console.WriteLine("I can make Sound");
+    }
+}
+public class Dog:Animal
+{
+    public override void Sound()
+    {
+        Console.WriteLine("I make dog sound");
+    }
+}
+public class Cat:Animal
+{
+    public override void Sound()
+    {
+        Console.WriteLine("i make cat sound");
+    }
+}
+public class Bird:Animal
+{
+    public override void Sound()
+    {
+        Console.WriteLine("i make bird sound");
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Animal d=new Dog();
+        d.Name = "Dog";
+        Console.WriteLine("Iam a " + d.Name);
+        d.Sound();
+        
+        
+        Animal c = new Cat();
+        c.Name = "Cat";
+        Console.WriteLine("Iam a " + c.Name);
+        c.Sound();
+
+        Animal B = new Bird();
+        B.Name = "Bird";
+        Console.WriteLine("Iam a " + B.Name);
+        B.Sound();
+       
+
+    }
+}
---
+++ calaculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+public class Calculator
+{
+    public double add(double number1,double number2)
+    { 
+        return number1 + number2; 
+    }
+    public double subtract(double number1,double number2) { 
+        return number1 - number2;
+    }
+    public double multiply(double number1,double number2) 
+    {
+        return number1 * number2; 
+    }
+    public double divide(double number1,double number2)
+    {
+        return number1 / number2;
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("enter first number");
+        double number1=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter second number");
+        double number2= Convert.ToInt32(Console.ReadLine());
+
+        Calculator calculator = new Calculator();
+        double add=calculator.add(number1,number2);
+        double sub=calculator.subtract(number1,number2);
+        double multiply=calculator.multiply(number1,number2);
+        double divide=calculator.divide(number1,number2);
+
+        Console.WriteLine("addition result " + add);
+        Console.WriteLine("substraction result " + sub);
+        Console.WriteLine("multiplication result "+multiply);
+        Console.WriteLine("division result "+divide);
+        
+    }
+}
