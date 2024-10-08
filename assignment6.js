97533603efe537c361aaf8a7c889207daebfd0d8
Asna-A <asna.a@geekywolf.com>
2024-10-08 11:27:05 +0530

    Update assignment6.js

--- assignment6.js
+++ assignment6.js
@@ -2,22 +2,22 @@ const prompt=require("prompt-sync")();
 
 function check_leapyear(){
     const year=prompt("Enter the year");
-    if(year!=null)
-    {
-        if((year%400)==0)
+    if (year == null || isNaN(year)) {
+        return false;
+    }
+    if((year%400)==0){
+        return true
+    }
+    if((year%100)!=0){
+        if((year%4)==0)
         {
             return true
         }
-        if((year%100)!=0)
-        {
-            if((year%4)==0)
-            {
-                return true
-            }
-        }
         return false
     }
+    return false
 }
+
 var result=check_leapyear();
 console.log(result)
 
